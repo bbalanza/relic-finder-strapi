515eae2ac766b0c0d195441527c36a9f6e51571d
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2021-10-21 00:38:54 -0500

    Remove duplicated production and staging files

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,1 +1,20 @@
-import '../staging/database'
+getProdConnections = (env) => {
+return {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+      },
+      options: {},
+    },
+  }
+}
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: getProdConnections(env), 
+});
--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,1 +1,14 @@
-import '../staging/plugins'
+getStoragePlugin = ({env}) => {
+return ({
+        provider: 'google-cloud-storage',
+        providerOptions: {
+            bucketName: `relic-finder-${env(_ENVIRONMENT)}-media`,
+            publicFiles: true,
+            uniform: true,
+            basePath: '',
+        },
+    })
+};
+module.exports = ({env}) => ({
+    upload: getStoragePlugin(env),
+})
