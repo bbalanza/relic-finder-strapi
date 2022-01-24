50cc5dfff733893a1cf904e3ac5ff25def75d48e
Jose Braulio Balanza Martinez <braulio-balanza@gmail.com>
2022-01-24 14:05:04 -0600

    Added tests for urlCreator helper method

--- src/api/qr-code/content-types/qr-code/helpers/index.js
+++ src/api/qr-code/content-types/qr-code/helpers/index.js
@@ -1,6 +1,8 @@
 import _ from "lodash"
 const urlCreator = (url, slug) => {
-    const slugString = parseInt(slug, 16)
+    const slugString = slug.toString(16).toUpperCase();
+    if(slug > _.parseInt('FFFF', 16))
+        throw "Slug is created than FFFF;"
     return url + _.padStart(slugString, 4, '0');
 }
 
